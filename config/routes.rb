b5d115bf1bc4cb45a820dc56370c0ba895cddb15
Braydan Clarke <braydanclarke@icloud.com>
2019-03-11 22:00:11 +0000

    Updated nav links using link_to

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     get 'signup', to: 'pages#signup'
-    get 'home', to: 'pages#home'
+    get '/', to: 'pages#home'
 end
